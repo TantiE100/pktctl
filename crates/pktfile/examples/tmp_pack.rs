6197d76563578ac6e7ce6f102b4e98e4d68abc67
Santi E. <cbbe.santiago.echeverria.te@unifranz.edu.bo>
2026-09-22 16:17:57 -0400

    chore: drop the temporary pktfile example used while debugging

--- crates/pktfile/examples/tmp_pack.rs
+++
@@ -1,5 +0,0 @@
-fn main() {
-    let mut args = std::env::args().skip(1);
-    let xml = std::fs::read_to_string(args.next().unwrap()).unwrap();
-    std::fs::write(args.next().unwrap(), pktfile::encode(&xml).unwrap()).unwrap();
-}
